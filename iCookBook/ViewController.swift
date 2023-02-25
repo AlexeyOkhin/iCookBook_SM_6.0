cfa9141a6de240054df51bc17beb672eb1789044
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-25 21:39:25 +0500

    Initial Commit

---
+++ iCookBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCookBook
+//
+//  Created by Djinsolobzik on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
