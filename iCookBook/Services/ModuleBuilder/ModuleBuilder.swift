288a1bb1c600e34d0f256408b932475b0ebe632b
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-26 15:30:11 +0500

    add builder for future

---
+++ iCookBook/Services/ModuleBuilder/ModuleBuilder.swift
@@ -0,0 +1,31 @@
+//
+//  ModuleBuilder.swift
+//  Tinkoff-Test
+//
+//  Created by Djinsolobzik on 04.02.2023.
+//
+
+import UIKit
+
+protocol Builder {
+    static func createHomeModule() -> UIViewController
+    //static func createDetailsModule() -> UIViewController
+}
+
+class ModuleBuilder: Builder {
+    static func createHomeModule() -> UIViewController {
+        let view = HomeViewController()
+        //let networkService = NetvorkService()
+//        let presenter = HomePresenter(view: view, networkService: networkService)
+//        view.presenter = presenter
+        return view
+    }
+
+//    static func createDetailsModule() -> UIViewController {
+//        let view = DetailsViewController()
+//        let networkService = NetvorkService()
+//        let presenter = DetailsPresenter(view: view, networkService: networkService, reciept: reciept)
+//        view.presenter = presenter
+//        return view
+//    }
+}
