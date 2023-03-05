3851d0a64584c3b5f958da3dd6e13cb80a7fd3c5
NSmolia <ipadsmolya@gmail.com>
2023-03-06 01:41:35 +0300

    Add favouriteTableViewCell

--- iCookBook/MainFlow/Favorites Module/FavoritesViewController.swift
+++ iCookBook/MainFlow/Favorites Module/FavoritesViewController.swift
@@ -1,16 +1,38 @@
-//
-//  FavoritesViewController.swift
-//  iCookBook
-//
-//  Created by Djinsolobzik on 26.02.2023.
-//
+
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
-
+class FavoritesViewController: UIViewController{
+ 
+    private let tableView: UITableView = {
+        let tableView = UITableView ()
+        tableView.register(FavouriteTableViewCell.self, forCellReuseIdentifier: FavouriteTableViewCell.favouriteCell)
+        return tableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
     }
-}
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            tableView.frame = view.bounds
+        }
+    }
+    extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: FavouriteTableViewCell.favouriteCell,
+                                                     for: indexPath)
+            
+            return cell
+         }
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 290
+        }
+    }
+
+    
---
+++ iCookBook/MainFlow/Favorites Module/FavouriteTableViewCell.swift
@@ -0,0 +1,120 @@
+//
+//  FavouriteTableViewCell.swift
+//  iCookBook
+//
+//  Created by Smolya on 06.03.2023.
+//
+
+import UIKit
+
+class FavouriteTableViewCell: UITableViewCell {
+        static let favouriteCell = "favouriteCellView"
+    
+    lazy var favouriteImageView: UIImageView = {
+        let favouriteImage = UIImageView()
+        favouriteImage.translatesAutoresizingMaskIntoConstraints = false
+        favouriteImage.contentMode = .scaleAspectFill
+        favouriteImage.layer.cornerRadius = 5
+        favouriteImage.backgroundColor = .yellow
+        favouriteImage.clipsToBounds = true
+        return favouriteImage
+    }()
+
+    lazy var favouriteTitleLabel: UILabel = {
+        let favouriteTitle = UILabel()
+        favouriteTitle.text = "Salad"
+        favouriteTitle.font = .preferredFont(forTextStyle: .headline)
+        favouriteTitle.numberOfLines = 0
+        favouriteTitle.textColor = .white
+        favouriteTitle.translatesAutoresizingMaskIntoConstraints = false
+        return favouriteTitle
+    }()
+
+
+    lazy var favoritesButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "bookmark")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
+        layer.masksToBounds = false
+        layer.shadowOpacity = 0.1
+        layer.shadowRadius = 4
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowColor = UIColor.black.cgColor
+
+        contentView.backgroundColor = .systemBackground
+        contentView.layer.cornerRadius = 8
+        clipsToBounds = true
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+    lazy var bottomGradientView = GradientView(from: .top, to: .bottom, startColor: .clear, endColor: .black)
+    lazy var topGradientView = GradientView(from: .bottom, to: .top, startColor: .clear, endColor: .black)
+
+
+    private func setupUI() {
+        bottomGradientView.translatesAutoresizingMaskIntoConstraints = false
+        topGradientView.translatesAutoresizingMaskIntoConstraints = false
+        favoritesButton.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(favouriteImageView)
+        addSubview(favouriteTitleLabel)
+        addSubview(favoritesButton)
+        favouriteImageView.addSubview(bottomGradientView)
+        favouriteImageView.addSubview(topGradientView)
+
+        let padding: CGFloat = 8
+
+        NSLayoutConstraint.activate([
+            bottomGradientView.heightAnchor.constraint(equalToConstant: 80),
+            bottomGradientView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            bottomGradientView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            bottomGradientView.bottomAnchor.constraint(equalTo: favouriteImageView.bottomAnchor),
+
+            
+            favouriteImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            favouriteImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            favouriteImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            favouriteImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding),
+
+
+            topGradientView.heightAnchor.constraint(equalToConstant: 80),
+            topGradientView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            topGradientView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            topGradientView.topAnchor.constraint(equalTo: favouriteImageView.topAnchor),
+
+            favouriteTitleLabel.centerYAnchor.constraint(equalTo: topGradientView.centerYAnchor),
+            favouriteTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            favouriteTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+
+            favoritesButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            favoritesButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding),
+            favoritesButton.heightAnchor.constraint(equalToConstant: 80),
+            favoritesButton.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+
+
+    func configureWith(recipe: Recipe) {
+        favouriteTitleLabel.text = recipe.title
+        ImageLoader2.shared.fetchImage(for: recipe) { image, id, error in
+            if let image {
+                self.favouriteImageView.image = image
+            }
+        }
+    }
+}
+
